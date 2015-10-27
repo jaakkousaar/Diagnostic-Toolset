3fbd984d9877e7e4c7732e235c1f9dcbb5756590
Jaak Kousaar <jaak.kousaar@gmail.com>
2015-10-28 00:12:50 +0200

    Renamed file_manager class, added ConvertAccessModeSpecifier method

---
+++ DataIO/include/file_manager_winapi.h
@@ -0,0 +1,35 @@
+// file_manager_winapi.h : header file
+// Created by Jaak Kõusaar @10.2015
+
+#ifndef DATAIO_FILEMANAGER_WINAPI_H_
+#define DATAIO_FILEMANAGER_WINAPI_H_
+
+
+#include <windows.h>
+
+#include <string>
+
+
+namespace dataio
+{
+	class FileManagerWinAPI
+	{
+	public:
+		explicit FileManagerWinAPI(
+									            const std::string& file_name,
+									            const char access_mode
+								              ) : file_name(file_name){};
+
+		inline const std::string& GetName() { return file_name; };
+
+	private:
+
+		const std::string file_name;
+    
+    const unsigned long ConvertAccessModeSpecifier(const char mode) const;
+
+	};
+}
+
+
+#endif // DATAIO_FILEMANAGER_WINAPI_H_
---
+++ DataIO/src/file_manager_winapi.cpp
@@ -0,0 +1,22 @@
+// file_manager_winapi.h : header file
+// Created by Jaak Kõusaar @10.2015
+
+#include "../include/file_manager_winapi.h"
+using namespace dataio;
+
+#include <string>
+using std::string;
+
+const unsigned long FileManagerWinAPI::ConvertAccessModeSpecifier(const char mode) const
+{
+  switch (mode)
+  {
+    case 'r':
+      return GENERIC_READ;
+    case 'w':
+      return GENERIC_WRITE;
+    case 'a':
+    default:
+      return GENERIC_ALL;  
+  }
+}
